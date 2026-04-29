5b6c06ecd445c39ab79f6adbb8c875d47624b8c2
Julio Rahman <julio.rahman@gmail.com>
2026-04-29 22:23:03 +0700

    Initial Commit

---
+++ Tiba/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tiba
+//
+//  Created by Julio Rahman on 29/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tiba/TibaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TibaApp.swift
+//  Tiba
+//
+//  Created by Julio Rahman on 29/04/26.
+//
+
+import SwiftUI
+
+@main
+struct TibaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
